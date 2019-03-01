fef1cef48c7ee41107350a9d9f0d82409fbeb92c
Sriram <sriram.baskar@soroco.com>
2019-03-01 13:14:56 +0530

    solved 41 problems

--- TRICOUNT.cpp
+++ TRICOUNT.cpp
@@ -1,26 +1,15 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include<stdio.h>
 int main()
 {
-	int t,i,max=0;
-	cin>>t;
-	int a[t];
-	for(i=0;i<t;i++)
-	{
-		cin>>a[i];
-		if(a[i]>max)
-			max=a[i];	
-	}
-	int ans[max],l=5;
-	ans[0]=1;
-	ans[1]=5;
-	for(i=2;i<max;i++)
-	{
-		ans[i] = ans[i-1] + l + ((i+1)*i/2);
-		l+=2;
-	}
-	for(i=0;i<t;i++)
-	{
-		cout<<ans[a[i]-1]<<endl;
-	}
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        unsigned long long n;
+        scanf("%llu",&n);
+        unsigned long long ans;
+        ans=(n*(n+2)*(2*n+1)/8);
+        printf("%llu\n",ans);
+    }
+    return 0;
 }
